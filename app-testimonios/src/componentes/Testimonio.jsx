65064440de98b2d0d9d2b782e5e3e873ef7687eb
FranciscoPardoo <105252301+FranciscoPardoo@users.noreply.github.com>
2023-08-15 12:58:51 -0300

    proyecto testimonios de fcc

---
+++ app-testimonios/src/componentes/Testimonio.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import "../estilos/testimonios.css";
+
+function Testimonio(props){
+    return(
+        <div className="contenedor-testimonio">
+            <img 
+            className="imagen-testimonio" 
+            src={`../src/assets/testimonio-${props.imagen}.png`}
+            alt="imagen"
+            />
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+                <p className="texto-testimonio"> {props.testimonio} </p>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonio;
